036a2c44cae7c0da405a933816b9ee41d16922b0
Francisco <fguzman53000@gmail.com>
2023-10-14 09:14:14 -0400

    deleteTask & modifyTask mutations

--- src/schema/task.ts
+++ src/schema/task.ts
@@ -37,7 +37,12 @@ builder.queryFields((t) => ({
         },
       })
 
-      return dbRequestResult?.tasks as Task[]
+      const sortedTasksByDate: Task[] = (dbRequestResult?.tasks as Task[]).sort(
+        (a, b) =>
+          new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime(),
+      )
+
+      return sortedTasksByDate
     },
   }),
 }))
@@ -63,19 +68,81 @@ builder.mutationFields((t) => ({
       const token = ctx.req?.req?.headers?.authorization?.split('Bearer ')[1]
       const { payload: tokenPayload } = tokenIsValid(token)
 
-      try {
-        const createdTask = await prisma.task.create({
-          ...query,
-          data: {
-            name: args.name,
-            userEmail: tokenPayload.email,
-          },
-        })
-        return createdTask
-      } catch (error) {
-        // @ts-ignore
-        throw new Error(error.message)
-      }
+      const createdTask = await prisma.task.create({
+        ...query,
+        data: {
+          name: args.name,
+          userEmail: tokenPayload.email,
+        },
+      })
+      return createdTask
+    },
+  }),
+  modifyTask: t.field({
+    type: 'Boolean',
+    errors: {
+      types: [Error],
+    },
+    authScopes: {
+      LOGGED_IN: true,
+      SUPERUSER: false,
+    },
+    args: {
+      completed: t.arg({
+        type: 'Boolean',
+        required: true,
+      }),
+      id: t.arg({
+        type: 'Int',
+        required: true,
+      }),
+    },
+    resolve: async (query, args, ctx) => {
+      // @ts-ignore
+      const token = ctx.req?.req?.headers?.authorization?.split('Bearer ')[1]
+      const { payload: tokenPayload } = tokenIsValid(token)
+
+      const updatedTask = await prisma.task.update({
+        where: {
+          userEmail: tokenPayload.email,
+          id: args.id,
+        },
+        data: {
+          completed: args.completed,
+        },
+      })
+
+      return updatedTask.completed
+    },
+  }),
+  deleteTask: t.prismaField({
+    type: 'Task',
+    errors: {
+      types: [Error],
+    },
+    authScopes: {
+      LOGGED_IN: true,
+      SUPERUSER: false,
+    },
+    args: {
+      id: t.arg({
+        type: 'Int',
+        required: true,
+      }),
+    },
+    resolve: async (query, parent, args, ctx) => {
+      // @ts-ignore
+      const token = ctx.req?.req?.headers?.authorization?.split('Bearer ')[1]
+      const { payload: tokenPayload } = tokenIsValid(token)
+
+      const deletedUser = await prisma.task.delete({
+        where: {
+          id: args.id,
+          userEmail: tokenPayload.email,
+        },
+      })
+
+      return deletedUser
     },
   }),
 }))
